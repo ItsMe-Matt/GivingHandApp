1d80a5e87ee8e228eaf8ef7c65f74259f0570359
ItsMe-Matt <mzhao42@my.bcit.ca>
2021-04-01 18:57:04 -0700

    Component 3 for Matt

--- ghv1/comps/CustomComponent3/index.js
+++ ghv1/comps/CustomComponent3/index.js
@@ -1,6 +1,6 @@
-function HeaderBlockUI({title="placeholder"},{R1="Yes"},{R2="No"}){
+function HeaderBlockUI({title="placeholder"},{R1="Yes"},{R2="No"},{text="lorem ipsum"}, {bgcolor="white"}){
     return <div style={{
-        backgroundColor:"white",
+        backgroundColor:{bgcolor},
         display: "flex",
         flexDirection: "column",
         width: "fit-content",
@@ -9,7 +9,7 @@ function HeaderBlockUI({title="placeholder"},{R1="Yes"},{R2="No"}){
         border: "2px black solid"
         }}>
         <h3 style={{textAlign: "center", padding: "1rem 0rem", margin: "0.5rem 1rem"}}>{title}</h3>
-        <span style={{width: "220px", padding:"0.5rem", margin:"0.5rem", textAlign:"center"}}>loresdads asas  askjd asjk asjkbasjh d sdads ad as dam ipsum</span>
+        <span style={{width: "220px", padding:"0.5rem", margin:"0.5rem", textAlign:"center"}}>{text}</span>
 
         <button type="button" style={{padding: "1rem 6rem", margin: "0.5rem 1rem", borderRadius: "8px"}}>{R1}</button>
         <button type="button" style={{padding: "1rem 6rem", margin: "0.5rem 1rem", marginBottom: "1.5rem", borderRadius: "8px"}}>No</button>
