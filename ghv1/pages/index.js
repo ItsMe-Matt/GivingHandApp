36d70ded233b0ce48cde99bd63abe9983f598cc1
ItsMe-Matt <mzhao42@my.bcit.ca>
2021-04-01 19:54:56 -0700

    final draft commit 6

--- ghv1/pages/index.js
+++ ghv1/pages/index.js
@@ -1,17 +1,10 @@
-
 import PageUI from "../comps/CustomComponent1"
 import ButtonUI from "../comps/CustomComponent2"
 import QuestionUI from "../comps/CustomComponent3"
 import MoneyButtonUI from "../comps/CustomComponent4"
 
-
-
-function Home() {
+export default function Home() {
   return (
    <div>homepage</div>
   )
 }
-
-
-export default Home;
-
