f4c29561eb347e29450ed78b613c466d4d7b3546
Aryanheravi <aheravitalemi@my.bcit.ca>
2021-04-19 23:39:09 -0700

    Big Changes

---
+++ .history/ghv1/pages/SupportUs/Sector/index_20210419224130.js
@@ -0,0 +1,29 @@
+import ButtonUI from '../../../Comps/ButtonUI'
+import styles from '../../../styles/SupportUs.module.css'
+
+
+function Home() {
+  return (<div className={styles.SupportUs}>
+    <div className={styles.bg}>
+       
+       <h1>Support Us</h1>
+       <br></br><br></br><br></br>
+       <h3>Choose a sector to help</h3>
+        <p>(Please select one)</p>
+    
+       <ButtonUI title= "Food" />
+       <br></br><br></br>
+       <ButtonUI  title= "Hygiene Products" />
+       <br></br><br></br>
+       <ButtonUI  title= "$50" />
+       <br></br><br></br>
+       <ButtonUI  title= "Other"/>
+       <br></br><br></br><br></br><br></br>
+    </div>
+  </div>
+
+    
+  )
+  }
+
+export default Home;
---
+++ .history/ghv1/pages/SupportUs/index_20210419214540.js
@@ -0,0 +1,32 @@
+import ButtonUI from '../../Comps/ButtonUI'
+import styles from '../../styles/main.module.css'
+
+
+function Home() {
+  return (<div className={styles.main}>
+    <div className={styles.bg}>
+       <h1>Support Us</h1>
+       <br></br><br></br><br></br>
+       <h4>Select volunteer hours (Please select one)</h4>
+    
+       <ButtonUI title= "1 Hour per day" />
+       <br></br><br></br>
+       <ButtonUI  title= "2 Hour per day" />
+       <br></br><br></br>
+       <ButtonUI  title= "3 Hour per day" />
+       <br></br><br></br>
+       <ButtonUI  title= "Other"/>
+       <br></br><br></br><br></br><br></br>
+       
+
+       
+
+      
+    </div>
+  </div>
+
+    
+  )
+  }
+
+export default Home;
