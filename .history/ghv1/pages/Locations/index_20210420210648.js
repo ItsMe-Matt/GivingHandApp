3b4dc4e844bef9efaa07c1e23303b2e8d5a9ff6d
Aryanheravi <aheravitalemi@my.bcit.ca>
2021-04-21 19:12:24 -0700

    Fixed the back button

---
+++ .history/ghv1/pages/Locations/index_20210420210648.js
@@ -0,0 +1,32 @@
+import ButtonUI from '../../Comps/ButtonUI'
+import NavbarUI from '../../Comps/NavBar'
+import styles from '../../styles/main.module.css'
+
+
+function Home() {
+  return (<div className={styles.main}>
+    <div className={styles.bg}>
+      <h1>LOCATIONS</h1>
+      <div className={styles.box}>
+       <br></br><br></br><br></br>
+    
+       <ButtonUI title= "Shelter" routeTo="/Shelter" />
+       <br></br><br></br>
+       <ButtonUI  title= "Clothing" routeTo="/Shelter" />
+       <br></br><br></br>
+       <ButtonUI  title= "Hygiene Products" />
+       <br></br><br></br>
+       <ButtonUI  title= "Food"/>
+       <br></br><br></br><br></br><br></br>
+
+       <NavbarUI />
+
+      </div>
+    </div>
+  </div>
+
+    
+  )
+  }
+
+export default Home;
