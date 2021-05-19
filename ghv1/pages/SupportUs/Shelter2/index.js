8f2f0e57e592b73fb0489fd0a08d1c5599841c6f
joshbreyes <jreyes46@my.bcit.ca>
2021-05-18 20:46:27 -0700

    first modal for shelter page complete

--- ghv1/pages/SupportUs/Shelter2/index.js
+++ ghv1/pages/SupportUs/Shelter2/index.js
@@ -1,11 +1,15 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
 import ButtonUI from '../../../Comps/ButtonUI'
 import SupportUs from '../../../Comps/SupportUs/index'
 import styles from '../../../styles/SupportUs.module.css'
-
+import Modal from '../../../Comps/Modal'
 import TopNavUI from '../../../Comps/TopNavUI'
 
 
 function Home() {
+  const [showModal, setShowModal] = useState(false);
+
   return (<div className={styles.SupportUs}>
     <div className={styles.bg}>
        <TopNavUI routeTo="/SupportUs/Sector2" />
@@ -17,10 +21,71 @@ function Home() {
        page="FourthSupport"
        />
     
-       <ButtonUI title= "Covenant House Vancouver" type="shadow" routeTo="/OverlayShelter1" />
-       <ButtonUI  title= "Aboriginal Shelter Vancouver" type="shadow"  routeTo="/OverlayShelter2" />
-       <ButtonUI  title= "Lookout Emergency Aid" type="shadow" routeTo="/OverlayShelter3" />
-       <ButtonUI  title= "Raincity Housing & Support Society" type="shadow" routeTo="/OverlayShelter4" />
+    <button 
+        className={styles.supportBtn}
+        onClick={() => setShowModal(true)}
+        >Covenant House Vancouver</button>
+        <Modal
+          onClose={() => setShowModal(false)}
+          show={showModal}
+        >
+          <p
+          className={styles.popP}
+          >
+          </p>
+          <br></br>
+          <ButtonUI title="Select" type="start" routeTo="/ThankYou13" />
+        </Modal>
+
+        <button 
+        className={styles.supportBtn}
+        onClick={() => setShowModal(true)}
+        >Aboriginal Shelter Vancouver</button>
+        <Modal
+          onClose={() => setShowModal(false)}
+          show={showModal}
+        >
+          <p
+          className={styles.popP}
+          >
+          </p>
+          <br></br>
+          <ButtonUI title="Select" type="start" routeTo="/ThankYou14" />
+        </Modal>
+
+        <button 
+        className={styles.supportBtn}
+        onClick={() => setShowModal(true)}
+        >Lookout Emergency Aid</button>
+        <Modal
+          onClose={() => setShowModal(false)}
+          show={showModal}
+        >
+          <p
+          className={styles.popP}
+          >
+          </p>
+          <br></br>
+          <ButtonUI title="Select" type="start" routeTo="/ThankYou15" />
+        </Modal>
+
+        <button 
+        className={styles.supportBtn}
+        onClick={() => setShowModal(true)}
+        >Raincity Housing & Support Society</button>
+        <Modal
+          onClose={() => setShowModal(false)}
+          show={showModal}
+        >
+          <p
+          className={styles.popP}
+          >
+          A home for every person' is a lofty goal. We believe it is 100% achievable and benefits all of us. And we are confident that if we all work together, we can achieve this goal sooner than we ever thought possible.
+          </p>
+          <br></br>
+          <ButtonUI title="Select" type="start" routeTo="/ThankYou16" />
+        </Modal>
+
        <br></br><br></br>
        <ButtonUI title="Continue" type="start" routeTo="/ThankYou" />
        <br></br>
